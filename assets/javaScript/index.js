ec032f248d841bd521602a12d8528b05329824ab
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-18 21:02:27 -0400

    starting number 8 off the readMe

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -1,1 +1,3 @@
-// lets begin
+// 8. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+
+// 9. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
