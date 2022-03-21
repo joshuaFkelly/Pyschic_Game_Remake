8ae85a45861fba5a6ee80eacde53eb0572c88cdf
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-21 17:43:04 -0400

    pseudo coded game and round object. going to create them now.

---
+++ assets/javaScript/thirdAttempt.js
@@ -0,0 +1,14 @@
+// parent object will be game
+// rounds will be extended off of game
+
+// game props:
+// win, loss, maxGuesses
+
+// game methods:
+// increment Wins, incrementLosses, setMaxGuesses, displayGame
+
+// round props:
+// letterToBeGuessed, guessesSoFar, guessesLeft
+
+// round methods:
+// getlettertobeguessed, saveGuesses, decrementMaxGuesses
